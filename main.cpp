354ba4ccfd59ed8074efad4ee9f0e79cb355e525
sentinel46 <altdaedroth@gmail.com>
2018-06-07 16:54:39 +0300

    Works for integer types and std::string

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@
 int main()
 {
     char c = -1;
-    print_ip(c);  // 255
+    my::print_ip(c);  // 255
 
     short s = 0;
-    print_ip(s);  // 0.0
+    my::print_ip(s);  // 0.0
 
     int i = 2130706433;
-    print_ip(i);  // 127.0.0.1
+    my::print_ip(i);  // 127.0.0.1
 
     long l = 8875824491850138409;
-    print_ip(l);  // 123.45.67.89.101.112.131.41
+    my::print_ip(l);  // 123.45.67.89.101.112.131.41
+
+    std::string str = "192.168.0.1";
+    my::print_ip(str);
+
+//    std::vector<int> v = {10, 20, 0, 1};
+//    my::print_ip(v);
 
     return 0;
 }
--- print_ip.h
+++ print_ip.h
@@ -2,42 +2,60 @@
 
 #include <type_traits>
 #include <iostream>
+#include <vector>
 
 namespace my
 {
-    template<class T, bool = std::is_integral<T>::value>
-    struct helper { };
+namespace h
+{
+template<class T, bool = std::is_integral<T>::value>
+struct helper { };
 
-    template<class T>
-    struct helper<T, true>
+template<class T>
+struct helper<T, true>
+{
+    static void print_ip(const T& t)
     {
-        static void print_ip(const T& t)
-        {
-            auto x = 0;
-            for (auto i = 0; i < sizeof(t); ++i)
-            {
-                if (i != 0)
-                    std::cout << ".";
-                //x = ((unsigned int)t >> (i << 3)) & 0xff;
-                x = (unsigned char*)&t[i];
-                std::cout << x;
-            }
-            std::cout << std::endl;
+        std::cout << "t = " << t << std::endl;
+        auto x = 0;
+        for (auto i = 0; i < sizeof(t); ++i) {
+            if (i != 0)
+                std::cout << ".";
+            x = ((unsigned int)t >> (8 * i)) & 0XFF;
+            std::cout << x;
         }
-    };
+        std::cout << std::endl;
 
-    template<class T>
-    struct helper<T, false>
-    {
-        static void print_ip(const T& t)
+        union bytes
         {
-            std::cout << t << std::endl;
+            unsigned char b[sizeof(t)];
+            T val;
+        } bi;
+        bi.val = t;
+        for (auto i = 1; i <= sizeof(t); ++i) {
+            if (i != 1)
+                std::cout << ".";
+            std::cout << (int)bi.b[sizeof(t) - i];
         }
-    };
-}
+        std::cout << std::endl;
+    }
+};
+
+template<class T>
+struct helper<T, false>
+{
+    static void print_ip(const T& t)
+    {
+        std::cout << t << std::endl;
+    }
+};
+
+}   // namespace h
 
 template<class T>
 void print_ip(const T& t)
 {
-    my::helper<T>::print_ip(t);
+    h::helper<T>::print_ip(t);
 }
+
+}   // namespace my
